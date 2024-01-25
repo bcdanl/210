9bd2c61babd1145ca685bbde6160aa8154101685
bcdanl <bchoe@geneseo.edu>
2024-01-24 23:17:02 -0500

    lec02

--- index-target-blank.R
+++ index-target-blank.R
@@ -1,23 +1,29 @@
-system("quarto render")
-# quarto::quarto_render("index.qmd")
-
-# Replace the values
-lec_no <- '01'
-lec_date <- '0123'
+# system("quarto render")
+quarto::quarto_render("index.qmd")
 
 library(stringr)
-old_content <- str_c('lec-', lec_no, '-2024-', lec_date, '.html"')
-new_content <- str_c(old_content, " ", 'target="_blank"') 
-
 
 # Read the HTML file
 html_as_text <- readLines("index.html", warn = FALSE)
 system(paste("rm", shQuote("index.html")))
 # Sys.sleep(2)
 
-html_as_text <- str_replace_all(html_as_text,
-                                old_content,
-                                new_content)
+
+lec_nos <- c('01', '02')
+lec_dates <- c('0123', '0125')
+
+for (i in 1:length(lec_nos)){
+  lec_no <- lec_nos[i]
+  lec_date <- lec_dates[i]
+  
+  old_content <- str_c('lec-', lec_no, '-2024-', lec_date, '.html"')
+  new_content <- str_c(old_content, " ", 'target="_blank"') 
+  
+  
+  html_as_text <- str_replace_all(html_as_text,
+                                  old_content,
+                                  new_content)
+}
 
 writeLines(html_as_text, 
            "index.html")
