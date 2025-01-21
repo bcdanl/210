02ac6ac7f7c596706b0338425143ddf91015655e
bcdanl <bchoe@geneseo.edu>
2025-01-21 16:53:08 -0500

    lec1

--- index-target-blank.R
+++ index-target-blank.R
@@ -9,20 +9,7 @@ system(paste("rm", shQuote("index.html")))
 # Sys.sleep(2)
 
 # Next lecture number & dates
-lec_dates <- c('0827', '0829', 
-               '0903', '0905',
-               '0910', '0912',
-               '0917', '0919',
-               '0924', '0926',
-               '1001', '1003',
-               '1010', '1017',
-               '1022', '1024',
-               '1029', '1031',
-               '1105', '1107',
-               '1112',
-               '1121',
-               '1203'
-               )
+lec_dates <- c('0122')
 lec_n <- length(lec_dates)
 
 lec_nos <- 1:lec_n
@@ -32,7 +19,7 @@ for (i in 1:length(lec_nos)){
   lec_no <- lec_nos[i]
   lec_date <- lec_dates[i]
   
-  old_content <- str_c('./danl-lec/danl-101-lec-', lec_no, '-2024-', lec_date, '.html')
+  old_content <- str_c('./danl-lec/danl-210-lec-', lec_no, '-2025-', lec_date, '.html')
   new_content <- str_c('#', 
                        '" onclick="', 
                        "window.open(", 
@@ -47,6 +34,3 @@ for (i in 1:length(lec_nos)){
 
 writeLines(html_as_text, 
            "index.html")
-
-# <a href="./econ-lec/econ-341-lec-01-2024-0826.html" class="title listing-title">Lecture 1</a>
-# <a href="#" onclick="window.open('./econ-lec/econ-341-lec-01-2024-0826.html'); return false;" class="title listing-title">Lecture 1</a>
