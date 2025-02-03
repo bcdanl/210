eba81cd815fd1db94201ed3a1a2916033da3b30b
bcdanl <bchoe@geneseo.edu>
2025-02-02 20:54:49 -0500

    lec5

--- index-target-blank.R
+++ index-target-blank.R
@@ -9,8 +9,9 @@ system(paste("rm", shQuote("index.html")))
 # Sys.sleep(2)
 
 # Next lecture number & dates
-lec_dates <- c('0122', '0124',
-               '0127', '0131')
+lec_dates <- c('0122', 
+               '0124', '0127', '0131',
+               '0203')
 lec_n <- length(lec_dates)
 
 lec_nos <- 1:lec_n
